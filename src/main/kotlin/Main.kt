066b7c59d2098d11e10f9012ca0de91cf3f3d55e
karol0797 <karol0797@gmail.com>
2022-05-29 19:39:52 +0200

    Updated main class.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ import api.ApiClient
 import config.ApiConfiguration
 import config.FileConfiguration
 import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
 import provider.CommentProvider
 import provider.PostProvider
 import provider.UserProvider
@@ -9,25 +10,32 @@ import service.FileReaderImpl
 import service.FileSaverImpl
 import service.PostSaver
 
-fun main() = runBlocking {
-    val fileReader = FileReaderImpl(System.getProperty("user.dir"))
-    val apiConfiguration = ApiConfiguration(fileReader).readConfiguration()
-    val fileConfiguration = FileConfiguration(fileReader).readConfiguration()
-    println("Configurations read.")
-    val apiClient = ApiClient(apiConfiguration)
-    val postApiService = apiClient.getPostApiService()
-    val userApiService = apiClient.getUserApiService()
-    val commentApiService = apiClient.getCommentApiService()
-    val userProvider = UserProvider(userApiService)
-    val commentProvider = CommentProvider(commentApiService)
-    val postProvider = PostProvider(postApiService, userProvider, commentProvider)
-    println("Getting posts from API...")
-    val posts = postProvider.getAllPosts()
-    println("Posts retrieved!")
-    posts?.let { posts ->
+class Main {
+    private val logger = KotlinLogging.logger(this.javaClass.simpleName)
+
+    fun main() = runBlocking {
+        val fileReader = FileReaderImpl(System.getProperty("user.dir"))
+        val apiConfiguration = ApiConfiguration(fileReader).readConfiguration()
+        val fileConfiguration = FileConfiguration(fileReader).readConfiguration()
+        logger.debug { "Configurations read." }
+        val apiClient = ApiClient(apiConfiguration)
+        val postApiService = apiClient.getPostApiService()
+        val userApiService = apiClient.getUserApiService()
+        val commentApiService = apiClient.getCommentApiService()
+        val userProvider = UserProvider(userApiService)
+        val commentProvider = CommentProvider(commentApiService)
+        val postProvider = PostProvider(postApiService, userProvider, commentProvider)
+        logger.debug { "Getting posts from API..." }
+        val posts = postProvider.getAllPosts()
+        logger.debug { "Posts retrieved!" }
         val fileSaver = FileSaverImpl(fileConfiguration.fileSavePath!!)
         val postSaver = PostSaver(fileSaver)
         postSaver.savePosts(posts)
+        logger.debug { "Job complete!" }
     }
-    println("Job complete!")
 }
+
+fun main() {
+    Main().main()
+}
+
